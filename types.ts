bdf0701f15a94c09f41292e169181965032c6a64
Ira Developer <iradeveloper2017@gmail.com>
2026-02-10 06:42:34 +0600

    Add files via upload

---
+++ types.ts
@@ -0,0 +1,13 @@
+
+export interface FamilyMember {
+  id: number;
+  role: string;
+  description: string;
+}
+
+export interface EducationInfo {
+  institution: string;
+  department: string;
+  status: string;
+  year: string;
+}
